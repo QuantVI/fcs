ad8de457d40a0f76624985e9be71b50b892bf3f9
QuantVI <berkeleyvi@gmail.com>
2025-01-31 16:49:50 +0100

    dnajgo 412 other issues

--- proj_lemon/littlelemon/littlelemon/urls.py
+++ proj_lemon/littlelemon/littlelemon/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
+    path("restaurant/", include('restaurant.urls')),
 ]
