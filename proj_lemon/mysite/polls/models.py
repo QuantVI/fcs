e1ba37ea6d2c5f3483f90286a9f3555812fc8e30
QuantVI <berkeleyvi@gmail.com>
2025-02-01 23:13:26 +0100

    more practice

--- proj_lemon/mysite/polls/models.py
+++ proj_lemon/mysite/polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
+
 # Create your models here.
 
 NOTES_1 = """
@@ -11,12 +14,21 @@ Each Choice is associated with a Question
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, 
     on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 NOTES_2 = """
 By running makemigrations, you’re telling Django that you’ve made some 
