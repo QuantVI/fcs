034c015c684f22b8d2f19e2b0ba92f8354a173c1
QuantVI <berkeleyvi@gmail.com>
2025-02-02 00:20:19 +0100

    django admin

--- proj_lemon/mysite/polls/admin.py
+++ proj_lemon/mysite/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+NOTES_1 = """
+But where’s our poll app? It’s not displayed on the admin index page.
+
+Only one more thing to do: we need to tell the admin that 
+Question objects have an admin interface. 
+To do this, open the polls/admin.py file, and edit it 
+(to import the Question object, and register it)
+"""
+
+from .models import Question
+
+admin.site.register(Question)
